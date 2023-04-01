5f052b07960c2afa7afe842366f4424791dbc13d
321niloy <niloyn670@gmail.com>
2023-04-01 16:06:07 +0600

    question setupappfile

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Content from './component/content/Content'
 import Nav from './component/Nav'
+import Question from './component/question/Question'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Nav></Nav>
       <Content></Content>
+      <Question></Question>
     </div>
   )
 }

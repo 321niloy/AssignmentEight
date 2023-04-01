6bffa579504209aa4ae64f3d48fb8b781b85807c
321niloy <niloyn670@gmail.com>
2023-04-01 16:11:19 +0600

    frist questione

--- src/component/question/Question.jsx
+++ src/component/question/Question.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 const Question = () => {
     return (
         <div>
+            <h1>Whats the different of props and State</h1>
+            <p>Answer: Props sends data one components to Other components and <br />
+               state save array,object, etc data
+            </p>
             
         </div>
     );

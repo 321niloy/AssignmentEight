7fefeedb468b3f2a279cd1242dccf6ec98a594de
321niloy <niloyn670@gmail.com>
2023-04-01 16:13:25 +0600

    Second questione

--- src/component/question/Question.jsx
+++ src/component/question/Question.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 const Question = () => {
     return (
         <div>
-            <h1>Whats the different of props and State</h1>
+            <h1>Whats the different of props and State??</h1>
             <p>Answer: Props sends data one components to Other components and <br />
                state save array,object, etc data
             </p>
+            <h1>WHAT does work useEffect without data load???</h1>
             
         </div>
     );

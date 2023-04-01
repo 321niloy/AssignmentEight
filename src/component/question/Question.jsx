aa5fedeb6470acf599ed43069e549088472ef0e4
321niloy <niloyn670@gmail.com>
2023-04-01 16:18:47 +0600

    Second answer

--- src/component/question/Question.jsx
+++ src/component/question/Question.jsx
@@ -8,6 +8,21 @@ const Question = () => {
                state save array,object, etc data
             </p>
             <h1>WHAT does work useEffect without data load???</h1>
+            <p>
+                Answer:Connecting to an external system
+                
+                Wrapping Effects in custom Hooks
+                Controlling a non-React widget
+               
+                Specifying reactive dependencies
+                Updating state based on previous state from an Effect
+                Removing unnecessary object dependencies
+                R emoving unnecessary function dependencies
+                Reading the latest props and state from an Effect
+                Displaying different content on the server and the client etc....
+
+
+            </p>
             
         </div>
     );
